6bfd47374a06e8b36f190b2bbf96b02ea45098e6
LalithaSathish <168298741+LalithaSathish@users.noreply.github.com>
2024-05-02 01:47:50 +0530

    Week1-Day2
    Day2 - Class actives and Home Assignment.

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class EdgeBrowser {
+	
+	public static void main(String[] args) {
+		Browser br = new Browser();
+		br.loadUrl();
+		System.out.println(br.launchBrowser("Edge"));
+	}
+
+}
